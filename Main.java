2880e54a853112c44e52a45ee9dcc75951e36e82
Emma <40247894+si-wang@users.noreply.github.com>
2020-11-30 23:19:01 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,4 +8,28 @@ class Main {
   public static void main(String[] args) {
     
   }
+
+  // Question 1
+  public static void printSkipArray(String[] input) {
+    // code for Question 1 here
+  }
+
+  // Question 2
+
+  // Question 3
+
+  // Question 4
+
+  // Question 5
+
+  // Question 6
+
+  // Question 7
+
+  // Question 8
+
+  // Question 9
+
+  // Question 10
+
 }
